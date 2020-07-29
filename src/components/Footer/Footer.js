3bdfe391ecd19c05974228dc2432d7f286d3cbbc
shivachevv <31926786+shivachevv@users.noreply.github.com>
2020-07-29 16:22:48 +0300

    Setting up Footer html and scss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png'
+import fb from '../../images/fb.png'
+import styles from './footer.module.scss'
+import defineLinks from '../../NavLinksProvider'
+import { UserContext } from '../../UserProvider'
 
 function Footer(props) {
+    const user = useContext(UserContext)
+
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className={styles.footerside}>
+                <img src={logo} alt=""></img>
+                <Link className="up" to="/">fantasy legends</Link>
+            </div>
+
+            <div className={styles.footercat}>
+                <h2 className="up">Categories</h2>
+                {defineLinks(user).map(link => {
+                    return <Link key={link.path} to={link.path}>{link.label}</Link>
+                })}
+            </div>
+            <div className={styles.footerside}>
+                <a className="up" href="https://www.facebook.com/groups/393007311341533/?ref=group_header" target="blank">Follow the legends</a>
+                <a href="https://www.facebook.com/groups/393007311341533/?ref=group_header" target="blank">
+                    <img src={fb} alt=""></img>
+                </a>
+            </div>
+        </footer>
     );
 }
 
