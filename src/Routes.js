0a51f56fb4a42742274cdec09fbea2adb6a10489
shivachevv <31926786+shivachevv@users.noreply.github.com>
2020-08-14 11:03:33 +0300

    updated guards for Login and Register as well

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Redirect, Route, Switch, useHistory, withRouter } from 'react-router-dom'
-import Loading from './components/Loading/Loading'
 import Home from './views/Home/Home'
 import Login from './views/Login/Login'
 import Register from './views/Register/Register'
@@ -9,92 +8,31 @@ import TeamDetails from './views/TeamDetails/TeamDetails'
 import EditTeam from './views/EditTeam/EditTeam'
 import AdminPanel from './views/AdminPanel/AdminPanel'
 import Standings from './views/Standings/Standings'
+import About from './views/About/About'
 import ErrorPage from './views/ErrorPage/ErrorPage'
 import AdminGuard from './RouteGuards/AdminGuard'
 import EditTeamGuard from './RouteGuards/EditTeamGuard'
-import About from './views/About/About'
+import CreateTeamGuard from './RouteGuards/CreateTeamGuard'
+import LoginGuard from './RouteGuards/LoginGuard'
+import RegisterGuard from './RouteGuards/RegisterGuard'
 
 
 const Routes = (props) => {
     const user = props.user
-    const [createTeamGuard, setCreateTeamGuard] = useState(true)
-    const [adminGuard, setAdminGuard] = useState(false)
-
-    const [adminLoading, setAdminLoading] = useState(true)
-    const [createTeamLoading, setCreateTeamLoading] = useState(true)
-
-    // CREATE TEAM GUARD
-    useEffect(() => {
-        // console.log('register check1')
-        async function createTeamGuardFn() {
-            // console.log('register check2')
-            if (user) {
-                // console.log('register check3')
-                const endpoint = user.email.split('.').join('-')
-                const response = await fetch(`https://softuni-react-final.firebaseio.com/users/${endpoint}.json`)
-                const data = await response.json()
-                if (!data) {
-                    // console.log('register check4')
-
-                    setCreateTeamGuard(false)
-                    setCreateTeamLoading(false)
-                } else if (data && !data.teamName) {
-                    // console.log('register check5')
-                    setCreateTeamGuard(false)
-                    setCreateTeamLoading(false)
-                }
-                else {
-                    // console.log('register check6')
-
-                    setCreateTeamGuard(true)
-                    setCreateTeamLoading(false)
-                }
-            } else {
-                // console.log('register check7')
-                setCreateTeamGuard(true)
-                setCreateTeamLoading(false)
-            }
-        }
-        createTeamGuardFn();
-    }, [user])
 
     return (
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/login" exact render={() => {
-                return user ? <Redirect to="/" /> : <Login />
-            }
-            } />
-            <Route path="/register" exact render={() => {
-                return user ? <Redirect to="/" /> : <Register />
-            }
-            } />
-            <Route path="/create-team" exact render={() => {
-                if (createTeamLoading) {
-                    console.log('final check1')
-                    return <Loading />
-                } else {
-                    console.log('final check1')
-
-                    return createTeamGuard ? <Redirect to="/" /> : <CreateTeam />
-                }
-            }
-            } />
             <Route path="/team-details/:id" exact component={TeamDetails} />
             <Route path="/standings" exact component={Standings} />
+            <Route path="/about" exact component={About} />           
 
+            <LoginGuard path="/login" component={Login} user={user} />
+            <RegisterGuard path="/register" component={Register} user={user} />
+            <CreateTeamGuard path="/create-team" component={CreateTeam} user={user} />
             <EditTeamGuard path="/edit-team/:id" component={EditTeam} user={user} />
             <AdminGuard path="/admin" component={AdminPanel} user={user} />
 
-            <Route path="/admin" exact render={() => {
-                if (adminLoading) {
-                    return <Loading />
-                } else {
-                    return adminGuard ? <AdminPanel /> : <Redirect to="/" />
-                }
-            }
-            } />
-            <Route path="/about" exact component={About} />
             <Route component={ErrorPage} />
         </Switch>
     )
